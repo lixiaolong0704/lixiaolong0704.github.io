408eb8723a38d7fe3e33b7ee3f31ac91d9aca314
lixiaolong1990 <sunleibo.home@gmail.com>
2015-05-21 17:33:15 +0800

    test2

---
+++ scripts/common.js
@@ -0,0 +1,20 @@
+﻿function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+$(function () {
+    //
+
+    $("div[part]").each(function () {
+        var $t = $(this);
+        $.ajax({
+            url: $t.attr("part"),
+            dataType: "text",
+            success: function (rst) {
+                $t.html(rst);
+            }
+        });
+    });
+});
